786770923fcc8e02f8aaccf1c3c043ed495701b5
aswu <andrewswu2000@gmail.com>
2019-01-31 12:36:15 -0500

    add TTTHuman and implement getMove()

---
+++ src/TTTHuman.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class TTTHuman extends Player{
+
+    public TTTHuman(String inName){
+        super(inName);
+    }
+    public String getMove(Board board){
+
+        System.out.println(board.toString());
+
+        System.out.print("Enter your move(1-9): ");
+        Scanner s = new Scanner(System.in);
+        String choice = s.nextLine();
+        while(!board.getEmptyLocs().contains(choice)) {
+            System.out.println("Please enter a valid move");
+            choice = s.nextLine();
+        }
+        return choice;
+    }
+}
