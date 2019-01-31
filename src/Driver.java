e00349ea2d86cc2efd2d2a2c0944eb98c84c514b
aswu <andrewswu2000@gmail.com>
2019-01-31 12:28:56 -0500

    add TTTHuman and implement getMove()

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,8 @@
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Bye");
-        System.out.println("test");
-        System.out.println("hi Andrew xd");
+        Board board = new TTTBoard();
+        Player player1 = new TTTHuman("X");
+        Player player2 = new TTTHuman("O");
+
     }
 }
