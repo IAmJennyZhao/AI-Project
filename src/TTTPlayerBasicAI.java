009b4a70f45385ef1af576bf6e08e1689ffdcdbe
aswu <andrewswu2000@gmail.com>
2019-02-07 12:52:17 -0500

    formatting changes

---
+++ src/TTTPlayerBasicAI.java
@@ -0,0 +1,12 @@
+/**
+ * Created by boles on 2/7/2019.
+ */
+import java.util.Random;
+public class TTTPlayerBasicAI extends Player{
+    public TTTPlayerBasicAI(String inName){
+        super(inName);
+    }
+    public String getMove(Board board){
+
+    }
+}
